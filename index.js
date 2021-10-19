7aff522c664d4433d815f4895e59b35aaea8ebe5
Vi Tran <contact.vitran@gmail.com>
2021-10-19 19:34:25 +1100

    Finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+//Canvas instructions: "Write a function that when called, declares a variable called bestCustomer in global scope 
+//and assigns it to be 'not bob'". - This is not possible??
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+} 
+
+function overwriteBestCustomer(newName) {
+    bestCustomer = newName;
+}
+
+const leastFavoriteCustomer = 'karen';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'susan';
+}
